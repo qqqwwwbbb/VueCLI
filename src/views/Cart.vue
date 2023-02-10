73d7065c24cffed72dfcf33436f94f6368632ba1
qqqwwwbbb <usecaut@gmail.com>
2023-02-10 16:47:07 +0700

    try delete

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,7 +1,12 @@
 <template>
   <h2>Cart</h2>
   <div>
-    <Product v-for="prod in cartArr" v-bind:key="prod.id" :prod-data="prod"></Product>
+    <Product v-for="prod in cartArr" v-bind:key="prod.id" :prod-data="prod">
+      {{ prod.price }}
+      <h2 @click="deleteCard(prod)" class="cross">+</h2>
+      <p>{{ fullSum }}</p>
+      <button @click="toOrder">Кнопка</button>
+    </Product>
   </div>
 </template>
 
@@ -9,6 +14,7 @@
 import axios from "axios";
 import Product from "@/components/Product";
 import router from "@/router";
+import cart from "@/views/Cart";
 export default {
   name: "Catalog",
   components: {Product},
@@ -51,5 +57,21 @@ export default {
 <style scoped>
 div {
   display: inline-block;
+}
+
+.cross {
+  transform: rotate(45deg);
+  cursor: pointer;
+}
+button {
+  color: #2c3e50;
+  width: 200px;
+  height: 32px;
+  margin: 10px 0;
+  font-size: 16px;
+  border: 2px solid #2c3e50;
+  border-radius: 5px;
+  background: rgba(234, 36, 248, 0.8);
+  cursor: pointer;
 }
 </style>
